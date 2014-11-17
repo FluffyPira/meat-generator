a1d0393d70cfdf933c3f0d8fa3abd2251986550e
Elizabeth Harper <FluffyPira@gmail.com>
2014-11-17 14:03:51 -0500

    It's somehow better

--- meatgen.rb
+++ meatgen.rb
@@ -1,4 +1,20 @@
 #!/usr/bin/env ruby
-first = File.readlines("textfiles/first.txt").shuffle.pop.strip
-last = File.readlines("textfiles/last.txt").shuffle.pop.strip
-puts "#{first} #{last}"
+def meatgen(count)
+  if count.is_number?
+    count.to_i.times do
+      first = File.readlines("textfiles/first.txt").shuffle.pop.strip
+      last = File.readlines("textfiles/last.txt").shuffle.pop.strip
+      puts "#{first} #{last}"
+    end
+  else
+    puts "Enter a valid number."
+  end
+end
+
+class String
+  def is_number?
+    true if Float(self) rescue false
+  end
+end
+
+meatgen(ARGV[0])
